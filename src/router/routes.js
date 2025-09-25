f8925d2f20d47024675c64be836f6a04605ba85c
Iasmin Cristina Flores de Toledo S. Prado <iasmincristina@gmail.com>
2025-09-25 09:48:35 -0300

    Criação do projeto e estruturação inicial das páginas.

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,8 +1,33 @@
 const routes = [
   {
     path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    component: () => import('layouts/Layout.vue'),
+    children: [
+      { 
+        path: '', 
+        component: () => import('pages/Home.vue') 
+      },
+      { 
+        path: '', 
+        component: () => import('pages/Atividades.vue') 
+      },
+      { 
+        path: '', 
+        component: () => import('pages/Ranking.vue') 
+      },
+      { 
+        path: '', 
+        component: () => import('pages/Turmas.vue') 
+      },
+      { 
+        path: '', 
+        component: () => import('pages/Badges.vue') 
+      },
+      { 
+        path: '', 
+        component: () => import('pages/Settings.vue') 
+      }
+    ]
   },
 
   // Always leave this as last one,

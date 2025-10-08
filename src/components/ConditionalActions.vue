0f3fbab5afd346b208562c70a66873e4e4abb6dd
jPales1 <joaopedropales@gmail.com>
2025-10-08 17:51:49 -0300

    feat: adiciona componente de exemplo ConditionalActions para gerenciamento de permissões via slots

---
+++ src/components/ConditionalActions.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="conditional-actions">
+    <slot 
+      :can="permissions.can"
+      :isProfessor="permissions.isProfessor.value"
+      :isAluno="permissions.isAluno.value"
+      :hasPermission="permissions.hasPermission"
+    />
+  </div>
+</template>
+
+<script setup>
+/**
+ * Componente reutilizável que fornece permissões via slots
+ * 
+ * Exemplo de uso:
+ * 
+ * <ConditionalActions v-slot="{ can, isProfessor, isAluno }">
+ *   <q-btn v-if="can.editarAtividade.value" label="Editar" />
+ *   <q-btn v-if="isAluno" label="Submeter" />
+ * </ConditionalActions>
+ */
+import { usePermissions } from 'src/composables/usePermissions'
+
+const permissions = usePermissions()
+</script>
+
+<style lang="scss" scoped>
+.conditional-actions {
+  display: contents;
+}
+</style>
+

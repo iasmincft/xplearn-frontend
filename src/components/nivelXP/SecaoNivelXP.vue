914092aa1134e3909a46ed882ee117d4a1efb5b2
Iasmin Cristina Flores de Toledo S. Prado <iasmincristina@gmail.com>
2025-12-08 10:20:43 -0300

    buscando nivel e xp do back

--- src/components/nivelXP/SecaoNivelXP.vue
+++ src/components/nivelXP/SecaoNivelXP.vue
@@ -1,17 +1,27 @@
 <template>
     <div class="row items-center q-gutter-sm">
-        <div class="text-subtitle1">Nível X</div>
+        <div class="text-subtitle1">Nível {{ userStore.currentUser.nivel }}</div>
         <q-linear-progress
             rounded
             size="15px"
-            :value="0.75"
+            :value="progress"
             color="primary"
             class="q-mt-sm col"
         />
-        <div class="text-right text-caption text-grey-5 q-mt-xs">750/1000 XP</div>
+        <div class="text-right text-caption text-grey-5 q-mt-xs">{{userStore.currentUser.xp}}/1000 XP</div>
     </div>
 </template>
 
-<script>
+<script setup>
+import { useUserStore } from 'src/stores/userStore';
+import { computed } from 'vue';
 
+const userStore = useUserStore();
+const progress = computed(() => {
+    const currentXp = Number(userStore.currentUser.xp) || 0;
+
+    if (currentXp === 0) return 0;
+
+    return Math.min(currentXp / 1000, 1);
+});
 </script>
